0ea4ef1ac1cc78efc1a680f8e8c9056c05ef9df2
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-05-05 21:23:50 +0900

    [level 0] Title: 문자열 섞기, Time: 9.38 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        char[] chars1 = str1.toCharArray();
-        char[] chars2 = str2.toCharArray();
-        for(int i=0;i<chars1.length;i++){
-            answer += chars1[i];
-            answer +=chars2[i];
+        for(int i=0;i<str1.length();i++){
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         }
         return answer;
     }
