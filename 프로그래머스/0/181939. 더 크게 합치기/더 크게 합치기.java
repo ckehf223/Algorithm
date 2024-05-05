7ebc1e17507901b3410590d68a24dfc8607d45ed
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-05-05 21:39:08 +0900

    [level 0] Title: 더 크게 합치기, Time: 7.04 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        if(Integer.parseInt(a+""+b) > Integer.parseInt(b+""+a)){
-            answer = Integer.parseInt(a+""+b);
-        }else if(Integer.parseInt(a+""+b) == Integer.parseInt(b+""+a)){
-            answer = Integer.parseInt(a+""+b);
-        }else{
-             answer = Integer.parseInt(b+""+a);
-        }
+        int aMax = Integer.parseInt(a+""+b);
+        int bMax = Integer.parseInt(b+""+a);
+        answer = aMax >= bMax ? aMax : bMax;
         return answer;
     }
 }
