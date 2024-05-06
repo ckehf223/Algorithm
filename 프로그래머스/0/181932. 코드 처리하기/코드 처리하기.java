ba0869a94dcc41765744ad95ee3763f80004bd99
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-05-06 21:23:47 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -2,27 +2,19 @@ class Solution {
     public String solution(String code) {
         String answer = "";
         int mode = 0;
-        String ret = "";
-        char[] chars = code.toCharArray();
-       for(int i=0;i<chars.length;i++) {
-			if(mode == 0) {
-				if(chars[i] == '1') {
-					mode = 1;
-				}else {
-					ret += i % 2 ==0? chars[i] : "";
-				}
-			}else {
-				if(chars[i] == '1') {
-					mode = 0;
-				}else {
-					ret += i % 2 !=0? chars[i] : "";
-				}
-			}
-		}
-        if(ret == ""){
-            ret = "EMPTY";
+        for(int i=0;i<code.length();i++){
+            if(code.charAt(i) == '1'){
+                mode = mode == 0 ? 1 : 0 ;
+                continue;
+            }
+            
+            if(i % 2 == mode){
+                answer += code.charAt(i);
+            }
         }
-        answer = ret;
+        if(answer == ""){
+            answer = "EMPTY";
+        }
         return answer;
     }
 }
