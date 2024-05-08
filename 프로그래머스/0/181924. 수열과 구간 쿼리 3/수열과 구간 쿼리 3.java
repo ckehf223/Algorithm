c411eb23a3e46d6a3091edab989002a0f2854292
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-05-08 23:57:41 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        for(int i=0;i<queries.length;i++){
-            for(int j=0;j<queries[i].length-1;j++){
-                int a = arr[queries[i][j+1]];
-                arr[queries[i][j+1]]=arr[queries[i][j]];
-                arr[queries[i][j]]=a;
-                // System.out.println(queries[i][j]);
-            }
+        for(int[] query:queries){
+            int a = query[0];
+            int b = query[1];
+            int c = arr[a];
+            arr[a] = arr[b];
+            arr[b] = c;
+            
         }
         return arr;
     }
