40f7c1b9db0fbf8e676f1bb2b250dfb76cdcfb48
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-06-08 21:50:04 +0900

    [level 1] Title: K번째수, Time: 0.41 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,13 +3,9 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 		for(int i=0;i<commands.length;i++) {
-			int start = commands[i][0];
-			int end = commands[i][1];
-			int index = commands[i][2];
-			int[] copy = new int[end-start+1];
-			System.arraycopy(array, start-1, copy, 0, end-start+1);
+			int[] copy = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
 			Arrays.sort(copy);
-			answer[i] = copy[index-1];
+			answer[i] = copy[commands[i][2]-1];
 		}
         return answer;
     }
