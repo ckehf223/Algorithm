0b36c209531923f4d166b96fdaf306caeec7ba4d
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-08-29 04:50:52 +0900

    [level 1] Title: 문자열 나누기, Time: 0.79 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,36 +1,23 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-      	char[] ch = s.toCharArray();
-		boolean flag = false;
+        char init = s.charAt(0);
 		int count = 0;
-		while(!flag) {
-			int temp1 = 1;
-			int temp2 = 0;
-			char a = ch[0];
-			if(ch.length <=2) {
+		for(char c : s.toCharArray()) {
+			if(count == 0) {
+				init = c;
+			}
+			if(init == c) {
+				count++;
+			}else {
+				count--;
+			}
+			if(count == 0) {
 				answer++;
-				flag = true;
-				break;
 			}
-			for(int i=1;i<ch.length;i++) {
-				if(temp1 == temp2) {
-					count += i;
-					ch = s.substring(count).toCharArray();
-					answer++;
-					break;
-				}
-				if(ch.length == i+1) {
-					answer++;
-					flag = true;
-					break;
-				}
-				if(ch[i] == a) {
-					temp1++;
-				}else {
-					temp2++;
-				}
-			}
+		}
+		if(count > 0) {
+			answer++;
 		}
         return answer;
     }
