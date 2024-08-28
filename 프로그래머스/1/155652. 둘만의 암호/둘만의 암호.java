01a762856661b9650245ee0699651b6bb34aee54
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-08-29 06:42:12 +0900

    [level 1] Title: 둘만의 암호, Time: 1.86 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -2,16 +2,10 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         char[] ch = new char[s.length()]; 
-		Map<Character, Boolean> map = new HashMap<>();
-		for (char c : skip.toCharArray()) {
-			map.put(c, true);
-		}
-
 		for (int i = 0; i < s.length(); i++) {
 			int count = index;
 			char c = s.charAt(i);
 			for (int j = 0; j < count; j++) {
-				
 				c = c == 'z' ? 'a':(char)(c+1);
 				if (skip.contains(String.valueOf(c))) {
 					count++;
