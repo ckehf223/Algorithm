9cf65cf60904eb8476d5f74dd4368ab2455464b1
ckehf223 <165243274+ckehf223@users.noreply.github.com>
2024-08-30 07:45:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.83 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,11 +3,10 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String,Integer> map = new HashMap<>();
+        
 		for(String data : completion) {
 			if(map.get(data) != null) {
-				int i = map.get(data);
-				map.remove(data);
-				map.put(data, i+1);
+				map.replace(data, map.get(data)+1);
 			}else {
 				map.put(data, 1);
 			}
@@ -15,13 +14,11 @@ class Solution {
 		
 		for(String data : participant) {
 			if(map.get(data) != null) {
-				int count = map.get(data);
-				if(count - 1 <0) {
+				if(map.get(data) - 1 <0) {
 					answer = data;
 					break;
-				}else {
-					map.replace(data, count-1);
 				}
+				map.replace(data, map.get(data)-1);
 			}else {
 				answer = data;
 				break;
