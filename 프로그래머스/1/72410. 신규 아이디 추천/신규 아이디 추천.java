3bac8e24f14cd2e974ac358a8468556a7196c58a
JaeKyeong Cha <165243274+ckehf223@users.noreply.github.com>
2024-09-20 21:21:54 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.39 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,41 +3,22 @@ class Solution {
     public String solution(String new_id) {
         StringBuilder sb = new StringBuilder();
 		new_id = new_id.toLowerCase().replaceAll("[^a-z0-9._-]", "");
-		ArrayList<Character> list = new ArrayList<>();
-		
-		for(int i=0;i<new_id.length();i++) {
-			if(new_id.charAt(i) == '.') {
-				if(list.isEmpty() || new_id.charAt(i) != list.get(list.size()-1)) {
-					list.add(new_id.charAt(i));
-				}
-			}else {
-				list.add(new_id.charAt(i));
-			}
-		}
-	
-		
-        while(!list.isEmpty() && list.get(0) == '.') {
-			list.remove(0);
-		}
-		while(!list.isEmpty() && list.get(list.size()-1) == '.') {
-			list.remove(list.size()-1);
-		}
-        if(list.isEmpty()) {
-			list.add('a');
+		new_id = new_id.replaceAll("[.]{2,}",".");
+		new_id = new_id.replaceAll("^[.]|[.]$","");
+		sb.append(new_id);
+
+		if(sb.length() <= 0) {
+			sb.append("a");
             return "aaa";
 		}
-		while(list.size() > 15) {
-			list.remove(list.size()-1);
+		while(sb.length() > 15) {
+			sb.deleteCharAt(sb.length()-1);
 		}
-        if(list.get(list.size()-1) == '.'){
-            list.remove(list.size()-1);
+		if(sb.charAt(sb.length()-1) == '.'){
+            sb.deleteCharAt(sb.length()-1);
         }
-		while(list.size() <= 2) {
-			list.add(list.get(list.size()-1));
-		}
-
-		for(char c : list) {
-			sb.append(c);
+		while(sb.length() <= 2) {
+			sb.append(sb.charAt(sb.length()-1));
 		}
         return sb.toString();
     }
