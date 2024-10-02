2fdcdb6282ede56300a3f030bab4507eb2c274b6
JaeKyeong Cha <165243274+ckehf223@users.noreply.github.com>
2024-10-02 20:31:50 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String ID = input.nextLine();
+		
+		System.out.println(ID+"??!");
+		
+	}
+}
