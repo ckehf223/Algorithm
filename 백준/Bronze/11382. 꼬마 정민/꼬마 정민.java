723dc08cd193b0631d02dcabe1e804a239dbf01e
JaeKyeong Cha <165243274+ckehf223@users.noreply.github.com>
2024-10-02 20:52:04 +0900

    [Bronze V] Title: 꼬마 정민, Time: 164 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String A = input.next();		
+		String B = input.next();		
+		String C = input.next();	
+		
+		System.out.println(Long.parseLong(A)+Long.parseLong(B)+Long.parseLong(C));
+	}
+}
+
