d009483d91ec4bacbca8a0fdd246c9f36b72d019
JaeKyeong Cha <165243274+ckehf223@users.noreply.github.com>
2024-10-07 00:37:53 +0900

    [Bronze V] Title: A+B - 4, Time: 116 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	String line = null;
+       while((line = br.readLine()) != null) {
+    	   String[] input = line.split(" ");
+    	   int A = Integer.parseInt(input[0]);
+    	   int B = Integer.parseInt(input[1]);
+    	   bw.write((A+B)+"\n");
+       }
+       bw.flush();
+       bw.close();
+    }
+}
